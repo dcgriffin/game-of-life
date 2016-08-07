5f3a69b313187d144075f57a334055e9f6d06c47
Daniel Griffin <sc15dcg@leeds.ac.uk>
2016-08-07 15:50:49 +0100

    Grid wraps around to deal with the grid edges

--- src/dg/gameoflife/GameOfLifeGUI.java
+++ src/dg/gameoflife/GameOfLifeGUI.java
@@ -3,7 +3,7 @@
 *
 * Purpose: Hobby Project.
 *
-* Date last modified: 03/08/16
+* Date last modified: 05/08/16
 *
 * Author: Daniel Griffin
 ******************************************************************************/
@@ -15,30 +15,34 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.text.Text;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import javafx.animation.KeyValue;
 
 
 public class GameOfLifeGUI extends Application {
-	
+
 	private Grid squareGrid;
 	private GridPane squareGridPane, buttonPane;
 	private VBox rootPane;
 	private Stage mainStage;
 	private Scene mainScene;
-	private Button startButton, stopButton;
+	private Button startButton;
 
 	// Displays the stage and creates the initial scene within it.
     @Override
-    public void start(Stage primaryStage) { 
+    public void start(Stage primaryStage) {
     	mainStage = primaryStage;
     	squareGridPane = new GridPane();
     	squareGrid = new Grid();
-    	
+
     	for (int x=0; x<40; x++) {
     		for (int y=0; y<40; y++) {
 		        Rectangle r = new Rectangle(15,15, Color.WHITE);
@@ -47,36 +51,37 @@ public class GameOfLifeGUI extends Application {
 		        squareGrid.add(r, x, y);
     		}
         }
-    	
+
     	squareGrid.turnCellBlack(20,20);
     	squareGrid.turnCellBlack(20,19);
     	squareGrid.turnCellBlack(21,19);
     	squareGrid.turnCellBlack(21,18);
     	squareGrid.turnCellBlack(19,18);
-    	
+
     	startButton = new Button("Start");
-        
-    	// When clicked it updates the grid after applying the rules.
+
+    	// When clicked it calls a function that continously updates the grid
+        // after applying the rules.
         startButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 GameOfLifeAlgorithm.createUpdatedGrid(squareGrid);
             }
         });
-        
+
         buttonPane = new GridPane();
         buttonPane.add(startButton,15,35);
 
         rootPane = new VBox(5);
         rootPane.getChildren().addAll(squareGridPane, buttonPane);
-        
+
         mainScene = new Scene(rootPane,700,700);
-        
+
         mainStage.setTitle("Conway's Game of Life");
         mainStage.setScene(mainScene);
-        mainStage.show();       
+        mainStage.show();
     }
-           
+
     public static void main(String[] args) {
     	launch(args);
     }
--- src/dg/gameoflife/Grid.java
+++ src/dg/gameoflife/Grid.java
@@ -4,7 +4,7 @@
 *
 * Purpose: Hobby Project.
 *
-* Date last modified: 03/08/16
+* Date last modified: 05/08/16
 *
 * Author: Daniel Griffin
 ******************************************************************************/
@@ -15,29 +15,29 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 public class Grid {
-	
+
 	private Rectangle[][] cells;
-	
-	// Constructor which creates a 30 by 30 array for the Rectangles.
+
+	// Constructor which creates a 40 by 40 array for the Rectangles.
 	public Grid() {
 		cells = new Rectangle[40][40];
 	}
-	
+
 	// Adds a Rectangle to the array of cells in the grid.
 	public void add(Rectangle r, int x, int y) {
 		cells[x][y] = r;
 	}
-	
+
 	// Turns a cell white to represent a dead state.
 	public void turnCellWhite (int x, int y) {
 		cells[x][y].setFill(Color.WHITE);
 	}
-	
+
 	// Turns a cell black to represent an alive state.
 	public void turnCellBlack (int x, int y) {
 		cells[x][y].setFill(Color.BLACK);
 	}
-	
+
 	// Returns 'true' if cell is alive and 'false' if it is dead.
 	public Boolean cellStatus(int x, int y){
 		if (cells[x][y].getFill() == Color.BLACK)
@@ -46,4 +46,17 @@ public class Grid {
 			return false;
 	}
 
+	// Returns true if there are any live cells left in the grid visible to the user.
+	public Boolean gridStatus() {
+		for (int x=0; x<40; x++) {
+    		for (int y=0; y<40; y++) {
+		        if (this.cellStatus(x, y) == true) {
+		        	System.out.println("True");
+		        	return true;
+		        }
+    		}
+        }
+		System.out.println("False");
+		return false;
+	}
 }
